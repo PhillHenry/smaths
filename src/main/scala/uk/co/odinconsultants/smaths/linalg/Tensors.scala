e7d17c150b5dee937517d61d10aee4d832d89053
PhillHenry <phillhenry@gmail.com>
2019-03-16 08:46:12 +0000

    Normalizations.

--- src/main/scala/uk/co/odinconsultants/smaths/linalg/Tensors.scala
+++ src/main/scala/uk/co/odinconsultants/smaths/linalg/Tensors.scala
@@ -1,16 +1,30 @@
 package uk.co.odinconsultants.smaths.linalg
 
 import scala.reflect.ClassTag
+import math._
 
 object Tensors {
 
   type Vec[T] = Array[T]
 
-  type Matrix[T] = Array[Array[T]]
+  type Matrix[T] = Vec[Vec[T]]
 
   def add[T : Numeric : ClassTag](xs: Vec[T], ys: Vec[T]): Vec[T] = {
     val op = implicitly[Numeric[T]]
     xs.zip(ys).map { case (x, y) => op.plus(x, y) }
+  }
+
+  def l1Normalize[T : Numeric : ClassTag](xs: Vec[T]): Vec[Double] = {
+    val op    = implicitly[Numeric[T]]
+    val total = op.toDouble(xs.sum)
+    xs.map(op.toDouble(_) / total)
+  }
+
+  def l2Normalize[T : Numeric : ClassTag](xs: Vec[T]): Vec[Double] = {
+    val op      = implicitly[Numeric[T]]
+    val squared = xs.map(x => op.times(x, x))
+    val total   = op.toDouble(squared.sum)
+    xs.map(op.toDouble(_) / pow(total, 0.5))
   }
 
 }
--- src/test/scala/uk/co/odinconsultants/smaths/linalg/TensorsSpec.scala
+++ src/test/scala/uk/co/odinconsultants/smaths/linalg/TensorsSpec.scala
@@ -10,8 +10,22 @@ class TensorsSpec extends WordSpec with Matchers {
     "add elements bitwise" in {
       add(Array(1, 2, 3), Array(3, 2, 1)) shouldBe Array(4, 4, 4)
     }
-    "should allow different Numeric types" ignore {
-      //add(Array(1d, 2d, 3d), Array(3, 2, 1)) shouldBe Array(4d, 4d, 4d)
+    "should allow different Numeric types" in {
+//      add(Array(1d, 2d, 3d), Array(3, 2, 1)) shouldBe Array(4d, 4d, 4d)
+    }
+  }
+
+  "L2 normalization" should {
+    val xs = Array(1, 2, 3, 4)
+    "produce elements that when squared add to 1.0" in {
+      l2Normalize(xs).map(x => x * x).sum shouldBe 1.0 +- 0.00001
+    }
+  }
+
+  "L1 normalization" should {
+    val xs = Array(1, 2, 3, 4)
+    "produce elements that add to 1.0" in {
+      l1Normalize(xs).sum shouldBe 1.0 +- 0.00001
     }
   }
 
