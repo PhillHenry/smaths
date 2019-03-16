7d1bc0617f85b45c7747aeaad01337ae92466691
PhillHenry <phillhenry@gmail.com>
2019-03-16 09:50:43 +0000

    Adding rows.

--- src/main/scala/uk/co/odinconsultants/smaths/linalg/Tensors.scala
+++ src/main/scala/uk/co/odinconsultants/smaths/linalg/Tensors.scala
@@ -9,9 +9,18 @@ object Tensors {
 
   type Matrix[T] = Vec[Vec[T]]
 
+  def toVec[T : Numeric : ClassTag](t: T*): Vec[T] = t.toArray
+
   def add[T : Numeric : ClassTag](xs: Vec[T], ys: Vec[T]): Vec[T] = {
     val op = implicitly[Numeric[T]]
     xs.zip(ys).map { case (x, y) => op.plus(x, y) }
+  }
+
+  def addRows[T : Numeric : ClassTag](xs: Matrix[T]): Vec[T] = {
+    val op    = implicitly[Numeric[T]]
+    val n     = xs.head.length
+    val empty = Array.fill(n)(op.zero)
+    xs.foldLeft(empty)(add)
   }
 
   def l1Normalize[T : Numeric : ClassTag](xs: Vec[T]): Vec[Double] = {
--- src/test/scala/uk/co/odinconsultants/smaths/linalg/TensorsSpec.scala
+++ src/test/scala/uk/co/odinconsultants/smaths/linalg/TensorsSpec.scala
@@ -6,9 +6,19 @@ class TensorsSpec extends WordSpec with Matchers {
 
   import Tensors._
 
+  val v1        = toVec(1, 2, 3)
+  val v2        = toVec(3, 2, 1)
+  val v1PlusV2  = toVec(4, 4, 4)
+
+  "Adding rows" should {
+    "combine all rows" in {
+      addRows(Array(v1, v2)) shouldBe v1PlusV2
+    }
+  }
+
   "Adding vectors" should {
     "add elements bitwise" in {
-      add(Array(1, 2, 3), Array(3, 2, 1)) shouldBe Array(4, 4, 4)
+      add(v1, v2) shouldBe v1PlusV2
     }
     "should allow different Numeric types" ignore {
 //      add(Array(1d, 2d, 3d), Array(3, 2, 1)) shouldBe Array(4d, 4d, 4d)
@@ -16,16 +26,14 @@ class TensorsSpec extends WordSpec with Matchers {
   }
 
   "L2 normalization" should {
-    val xs = Array(1, 2, 3, 4)
     "produce elements that when squared add to 1.0" in {
-      l2Normalize(xs).map(x => x * x).sum shouldBe 1.0 +- 0.00001
+      l2Normalize(v1).map(x => x * x).sum shouldBe 1.0 +- 0.00001
     }
   }
 
   "L1 normalization" should {
-    val xs = Array(1, 2, 3, 4)
     "produce elements that add to 1.0" in {
-      l1Normalize(xs).sum shouldBe 1.0 +- 0.00001
+      l1Normalize(v1).sum shouldBe 1.0 +- 0.00001
     }
   }
 
