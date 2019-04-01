1ac95b08f1a24759617abb392ffc7b2dd43aacb6
PhillHenry <phillhenry@gmail.com>
2019-04-01 16:08:11 +0100

    Can use parallel collections now.

--- src/main/scala/uk/co/odinconsultants/smaths/stats/Averages.scala
+++ src/main/scala/uk/co/odinconsultants/smaths/stats/Averages.scala
@@ -1,18 +1,19 @@
 package uk.co.odinconsultants.smaths.stats
 
 import math.pow
+import scala.collection.GenIterable
 
 object Averages {
 
-  def mean[T : Numeric](xs: Seq[T]): Double =
+  def mean[T : Numeric](xs: GenIterable[T]): Double =
     implicitly[Numeric[T]].toDouble(xs.sum) / xs.size
 
-  def variance[T : Numeric](xs: Seq[T], bessel: Boolean): Double = {
+  def variance[T : Numeric](xs: GenIterable[T], bessel: Boolean): Double = {
     val mu  = mean(xs)
     val n   = if (bessel) xs.size - 1 else xs.size
     xs.map { x => pow(implicitly[Numeric[T]].toDouble(x) - mu, 2) }.sum / n
   }
 
-  def stdDev[T : Numeric](xs: Seq[T], bessel: Boolean): Double =  pow(variance(xs, bessel), 0.5)
+  def stdDev[T : Numeric](xs: GenIterable[T], bessel: Boolean): Double =  pow(variance(xs, bessel), 0.5)
 
 }
